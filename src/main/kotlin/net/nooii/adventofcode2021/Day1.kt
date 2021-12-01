32390ded35344feaf529095eac3b61426a82cba3
Nooii <zbobo@seznam.cz>
2021-12-01 13:17:58 +0100

    Setup

---
+++ src/main/kotlin/net/nooii/adventofcode2021/Day1.kt
@@ -0,0 +1,17 @@
+package net.nooii.adventofcode2021
+
+/**
+ * Created by Nooii on 01.12.2021
+ */
+class Day1 {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args : Array<String>) {
+            println("Hello")
+        }
+
+    }
+
+}
