ec19e41a7ff2eeb42220c8848b12a02fb6ff670a
blackmailcz <zbobo@seznam.cz>
2022-12-02 13:54:18 +0100

    Day2

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2022/Day2.kt
@@ -0,0 +1,98 @@
+package net.nooii.adventofcode.aoc2022
+
+import net.nooii.adventofcode.aoc2022.Day2.RPS.*
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+
+class Day2 {
+
+    private enum class RPS(val score: Int) {
+        ROCK(1),
+        PAPER(2),
+        SCISSORS(3);
+
+        fun strongAgainst(): RPS {
+            return when (this) {
+                ROCK -> SCISSORS
+                PAPER -> ROCK
+                SCISSORS -> PAPER
+            }
+        }
+
+        fun weakAgainst(): RPS {
+            return when (this) {
+                ROCK -> PAPER
+                PAPER -> SCISSORS
+                SCISSORS -> ROCK
+            }
+        }
+    }
+
+    private class Round(
+        val opponent: RPS,
+        val you: RPS
+    ) {
+
+        fun computeScore(): Int {
+            val matchScore = when {
+                you == opponent -> 3
+                you.strongAgainst() == opponent -> 6
+                else -> 0
+            }
+            return matchScore + you.score
+        }
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2022).loadStrings("Day2Input")
+            part1(input)
+            part2(input)
+        }
+
+        private fun part1(input: List<String>) {
+            val rounds = input.map { line ->
+                val parts = line.split(" ")
+                val opponent = parseOpponent(parts[0])
+                val you = when (parts[1]) {
+                    "X" -> ROCK
+                    "Y" -> PAPER
+                    "Z" -> SCISSORS
+                    else -> error("Unsupported ${parts[1]}")
+                }
+                Round(opponent, you)
+            }
+            printTotalScore(rounds)
+        }
+
+        private fun part2(input: List<String>) {
+            val rounds = input.map { line ->
+                val parts = line.split(" ")
+                val opponent = parseOpponent(parts[0])
+                val you = when (parts[1]) {
+                    "X" -> opponent.strongAgainst()
+                    "Y" -> opponent
+                    "Z" -> opponent.weakAgainst()
+                    else -> error("Unsupported ${parts[1]}")
+                }
+                Round(opponent, you)
+            }
+            printTotalScore(rounds)
+        }
+
+        private fun parseOpponent(part: String): RPS {
+            return when (part) {
+                "A" -> ROCK
+                "B" -> PAPER
+                "C" -> SCISSORS
+                else -> error("Unsupported $part")
+            }
+        }
+
+        private fun printTotalScore(rounds: List<Round>) {
+            println(rounds.sumOf { it.computeScore() })
+        }
+    }
+}
