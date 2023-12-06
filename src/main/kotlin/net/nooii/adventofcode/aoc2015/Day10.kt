7510feacad7edac38e94256b22f342445f6126bc
blackmailcz <marek.teuchner@gendigital.com>
2023-12-06 01:43:20 +0100

    2015: Day10

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2015/Day10.kt
@@ -0,0 +1,69 @@
+package net.nooii.adventofcode.aoc2015
+
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+import net.nooii.adventofcode.helpers.NonNullMap
+import java.lang.StringBuilder
+
+class Day10 {
+
+    private data class Key(val c: Char, val n: Int)
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2015).loadStrings("Day10Input").first()
+            part1(input)
+            // Runtime ~ 1 sec
+            part2(input)
+        }
+
+        private fun part1(input: String) {
+            solution(input, 40)
+        }
+
+        private fun part2(input: String) {
+            solution(input, 50)
+        }
+
+        private fun solution(input: String, repeats: Int) {
+            // Works fast enough for given input and repeats
+            val baseMap: NonNullMap<Key, String> = createBaseMap()
+            var str = input
+            repeat(repeats) {
+                var n = 0
+                var last: Char? = null
+                val nextStr = StringBuilder() // Using StringBuilder speeds up the process
+                for (i in str.indices) {
+                    when (last) {
+                        null -> {
+                            last = str[i]
+                            n++
+                        }
+                        str[i] -> n++
+                        else -> {
+                            nextStr.append(baseMap[Key(last, n)])
+                            last = str[i]
+                            n = 1
+                        }
+                    }
+                    if (i == str.length - 1) {
+                        nextStr.append(baseMap[Key(last, n)])
+                    }
+                }
+                str = nextStr.toString()
+            }
+            println(str.length)
+        }
+
+        private fun createBaseMap(): NonNullMap<Key, String> {
+            val baseMap = NonNullMap(mutableMapOf<Key, String>())
+            for (char in setOf('1', '2', '3')) {
+                for (n in 1..3) {
+                    baseMap[Key(char, n)] = "$n$char"
+                }
+            }
+            return baseMap
+        }
+    }
+}
