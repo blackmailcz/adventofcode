906c50df2e2ce8e7f6cfeb94c10703ff821a536d
blackmailcz <marek.teuchner@gendigital.com>
2023-12-14 01:09:56 +0100

    2015: Day25

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2015/Day25.kt
@@ -0,0 +1,38 @@
+package net.nooii.adventofcode.aoc2015
+
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+import java.awt.Point
+
+class Day25 {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2015).loadStrings("Day25Input")
+            val point = processInput(input)
+            part1(point)
+        }
+
+        private fun part1(point: Point) {
+            var n = 20151125L
+            val iterations = getNumberOfIterations(point)
+            for (i in 0 until iterations - 1) {
+                n = n * 252533L % 33554393L
+            }
+            println(n)
+        }
+
+        private fun getNumberOfIterations(point: Point): Long {
+            // Formula for sum of sequence of numbers
+            val n = (point.y + point.x - 2).toLong()
+            return n + n * (n - 1) / 2 + point.y
+        }
+
+        private fun processInput(input: List<String>): Point {
+            val (x, y) = Regex("(\\d+)").findAll(input.first()).map { it.value.toInt() }.toList()
+            return Point(x, y)
+        }
+    }
+}
