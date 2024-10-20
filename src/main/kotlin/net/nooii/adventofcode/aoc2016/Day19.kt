6eb433b583e1bd2ace404c43fd156b4f16686ad2
blackmailcz <marek.teuchner@gendigital.com>
2024-10-20 07:31:45 +0200

    2016: Day19

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2016/Day19.kt
@@ -0,0 +1,42 @@
+package net.nooii.adventofcode.aoc2016
+
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+import java.util.*
+
+class Day19 {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2016).loadInts("Day19Input").first()
+            part1(input)
+            part2(input)
+        }
+
+        private fun part1(input: Int) {
+            val elves = TreeSet(IntRange(1, input).toSet())
+            var currentElf = 1
+            while (elves.size > 1) {
+                val victim = elves.higher(currentElf) ?: elves.higher(0) ?: break
+                elves.remove(victim)
+                currentElf = elves.higher(currentElf) ?: elves.higher(0) ?: break
+            }
+            println(currentElf)
+        }
+
+        private fun part2(input: Int) {
+            // Brute force cycle, 30 minutes run time
+            val elves = IntRange(1, input).toMutableList()
+            var currentElfIndex = 0
+            while (elves.size > 1) {
+                val oppositeElfIndex = (currentElfIndex + elves.size / 2) % elves.size
+                elves.removeAt(oppositeElfIndex)
+                currentElfIndex =
+                    (if (oppositeElfIndex < currentElfIndex) currentElfIndex else (currentElfIndex + 1)) % elves.size
+            }
+            println(elves.first())
+        }
+    }
+}
