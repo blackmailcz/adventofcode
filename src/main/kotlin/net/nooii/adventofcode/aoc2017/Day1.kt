3276b52c63c819898062dfa4a23477c80ca51e24
blackmailcz <marek.teuchner@gendigital.com>
2024-10-22 03:53:39 +0200

    2017: Day1

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2017/Day1.kt
@@ -0,0 +1,34 @@
+package net.nooii.adventofcode.aoc2017
+
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+
+class Day1 {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2017).loadStrings("Day1Input")
+            val sequence = input.first()
+            part1(sequence)
+            part2(sequence)
+        }
+
+        private fun part1(input: String) {
+            solution(input) { it + 1 }
+        }
+
+        private fun part2(input: String) {
+            solution(input) { it + input.length / 2 }
+        }
+
+        private fun solution(input: String, next: (current: Int) -> Int) {
+            val sum = input.withIndex().sumOf { (index, char) ->
+                val nextChar = input[next.invoke(index) % input.length]
+                if (nextChar == char) nextChar.digitToInt() else 0
+            }
+            println(sum)
+        }
+    }
+}
--- src/main/kotlin/net/nooii/adventofcode/helpers/AoCYear.kt
+++ src/main/kotlin/net/nooii/adventofcode/helpers/AoCYear.kt
@@ -7,8 +7,8 @@ enum class AoCYear(val resourcePath: String) {
 
     AOC_2015("aoc2015"),
     AOC_2016("aoc2016"),
+    AOC_2017("aoc2017"),
     AOC_2021("aoc2021"),
     AOC_2022("aoc2022"),
     AOC_2023("aoc2023")
-
 }
