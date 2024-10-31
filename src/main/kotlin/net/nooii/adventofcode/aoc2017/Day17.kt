cc627a5fdab2c5559289d244d9acfbd2ad0de935
blackmailcz <marek.teuchner@gendigital.com>
2024-10-31 17:14:00 +0100

    2017: Day17

---
+++ src/main/kotlin/net/nooii/adventofcode/aoc2017/Day17.kt
@@ -0,0 +1,40 @@
+package net.nooii.adventofcode.aoc2017
+
+import net.nooii.adventofcode.helpers.AoCYear
+import net.nooii.adventofcode.helpers.InputLoader
+
+class Day17 {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = InputLoader(AoCYear.AOC_2017).loadInts("Day17Input").first()
+            part1(input)
+            part2(input)
+        }
+
+        private fun part1(stepSize: Int) {
+            val rotations = 2017
+            val buffer = mutableListOf(0)
+            var position = 0
+            for (i in 1..rotations) {
+                position = (position + stepSize) % buffer.size + 1
+                buffer.add(position, i)
+            }
+            println(buffer[buffer.indexOf(rotations) + 1])
+        }
+
+        private fun part2(stepSize: Int) {
+            var valueAfterZero = 0
+            var position = 0
+            for (i in 1..50_000_000) {
+                position = (position + stepSize) % i + 1
+                if (position == 1) {
+                    valueAfterZero = i
+                }
+            }
+            println(valueAfterZero)
+        }
+    }
+}
