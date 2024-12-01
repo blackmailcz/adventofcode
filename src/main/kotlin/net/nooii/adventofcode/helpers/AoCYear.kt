bd9eb8c878841d6f5c2f5394d02bae0dc7d2317a
blackmailcz <marek.teuchner@gendigital.com>
2024-12-01 06:18:13 +0100

    2024: Day1

--- src/main/kotlin/net/nooii/adventofcode/helpers/AoCYear.kt
+++ src/main/kotlin/net/nooii/adventofcode/helpers/AoCYear.kt
@@ -10,5 +10,6 @@ enum class AoCYear(val resourcePath: String) {
     AOC_2017("aoc2017"),
     AOC_2021("aoc2021"),
     AOC_2022("aoc2022"),
-    AOC_2023("aoc2023")
+    AOC_2023("aoc2023"),
+    AOC_2024("aoc2024")
 }
